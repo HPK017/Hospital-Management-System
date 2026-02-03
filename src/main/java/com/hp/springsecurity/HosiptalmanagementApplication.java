080e9a0def60cba991029c156940a873b7d80e6c
HPK017 <parelharsh0250@gmail.com>
2026-02-03 20:56:43 +0530

    initial commit

---
+++ src/main/java/com/hp/springsecurity/HosiptalmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hp.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosiptalmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosiptalmanagementApplication.class, args);
+	}
+
+}

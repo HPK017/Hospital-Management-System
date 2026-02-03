da0d83e9d4a0b45a408c3b42e23db1b5de2de6cc
HPK017 <parelharsh0250@gmail.com>
2026-02-03 21:26:09 +0530

    Blood group dto added

--- src/main/java/com/hp/springsecurity/dto/BloodGroupResponseEntity.java
+++ src/main/java/com/hp/springsecurity/dto/BloodGroupResponseEntity.java
@@ -1,4 +1,19 @@
 package com.hp.springsecurity.dto;
 
+import com.hp.springsecurity.type.BloodGroupType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class BloodGroupResponseEntity {
+
+    private BloodGroupType bloodGroupType;
+    private Long count;
+
 }

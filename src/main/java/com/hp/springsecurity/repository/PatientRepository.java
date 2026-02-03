be12dd642db0a0bdb7abd2ca083ba5f242cbe532
HPK017 <parelharsh0250@gmail.com>
2026-02-03 21:31:26 +0530

    Blood group dto added

---
+++ src/main/java/com/hp/springsecurity/repository/PatientRepository.java
@@ -0,0 +1,44 @@
+package com.hp.springsecurity.repository;
+
+import com.hp.springsecurity.dto.BloodGroupResponseEntity;
+import com.hp.springsecurity.entity.Patient;
+import com.hp.springsecurity.type.BloodGroupType;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByName(String name);
+
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+
+    List<Patient> findByBirthDateBetween(LocalDate startDate, LocalDate endDate);
+
+    List<Patient> findByNameContaingOrderByIdDesc(String query);
+
+    @Query("Select p from Patient p where p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup")BloodGroupType bloodGroupType);
+
+    @Query("select p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+
+    @Query("select new com.hp.springsecurity.dto.BloodGroupResponseEntity(p.bloodGroup, "+
+            "count(p)) from Patient p group by p.bloodGroup")
+    List<BloodGroupResponseEntity> countEachBloodGroupType();
+
+    @Query(value = "select * from patient", nativeQuery = true)
+    Page<Patient> findAllPatients(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
+    int updateNameWithId(@Param("name") String name, @Param("id") Long id);
+
+}

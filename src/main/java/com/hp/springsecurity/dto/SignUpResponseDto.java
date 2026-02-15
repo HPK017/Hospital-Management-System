7b35f0c3ab4dc5a0a1b3e8ce2b687254d40e2aec
HPK017 <parelharsh0250@gmail.com>
2026-02-15 11:40:56 +0530

    Hospital Management - Login and signup route.

--- src/main/java/com/hp/springsecurity/dto/SignUpResponseDto.java
+++ src/main/java/com/hp/springsecurity/dto/SignUpResponseDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginResponseDto {
-    String jwt;
-    Long userId;
+public class SignUpResponseDto {
+    private Long id;
+    private String username;
 }

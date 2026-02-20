bf673380293beaeec495f1eb8d09c2ee16425508
HPK017 <parelharsh0250@gmail.com>
2026-02-20 19:42:40 +0530

    Hospital Management - refresh token creation

--- src/main/java/com/hp/springsecurity/repository/RefreshTokenRepository.java
+++ src/main/java/com/hp/springsecurity/repository/RefreshTokenRepository.java
@@ -3,6 +3,9 @@ package com.hp.springsecurity.repository;
 import com.hp.springsecurity.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
+    Optional<RefreshToken> findByToken(String token);
 }
--- src/main/java/com/hp/springsecurity/service/RefreshTokenService.java
+++ src/main/java/com/hp/springsecurity/service/RefreshTokenService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,5 +27,16 @@ public class RefreshTokenService {
         return refreshTokenRepository.save(refreshToken);
     }
 
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException(token.getToken() + " Refresh Token was expired. Please make a new sign in request");
+        }
+        return token;
+    }
 
 }

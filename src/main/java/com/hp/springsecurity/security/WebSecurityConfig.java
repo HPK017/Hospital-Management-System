d04447efe16c948a0f3b7978420fca6d3412e8d3
HPK017 <parelharsh0250@gmail.com>
2026-02-24 20:16:02 +0530

    Resolved application.yml from repo

--- src/main/java/com/hp/springsecurity/security/WebSecurityConfig.java
+++ src/main/java/com/hp/springsecurity/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.hp.springsecurity.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,22 +16,45 @@ public class WebSecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(csrfConfig -> csrfConfig.disable())
-                .sessionManagement(sessionConfig ->
-                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/public/**", "/auth/**").permitAll()
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .csrf(csrfConfig -> csrfConfig.disable())
+//                .sessionManagement(sessionConfig ->
+//                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(auth -> auth
+//                                .requestMatchers("/public/**", "/auth/**").permitAll()
+////                        .requestMatchers("/admin/**").hasRole("ADMIN")
+////                        .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
+//                                .anyRequest().authenticated()
+//                )
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+////                .formLogin(Customizer.withDefaults());
+//        return httpSecurity.build();
+//    }
+
+
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+            .csrf(csrfConfig -> csrfConfig.disable())
+            .sessionManagement(sessionConfig ->
+                    sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/public/**", "/auth/**").permitAll()
 //                        .requestMatchers("/admin/**").hasRole("ADMIN")
 //                        .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
-                                .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                            .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .oauth2Login(oAuth2 -> oAuth2.failureHandler(
+                    ((request, response, exception) -> {
+                        System.out.println("OAuth2 error: {}"+ exception.getMessage());
+                    })
+            ));
 //                .formLogin(Customizer.withDefaults());
-        return httpSecurity.build();
-    }
+    return httpSecurity.build();
+}
 
 
 }

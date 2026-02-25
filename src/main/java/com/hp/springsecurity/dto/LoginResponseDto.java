281db937a6208cae5d1a79b95d087ae0c3d9a4a4
HPK017 <parelharsh0250@gmail.com>
2026-02-25 22:00:54 +0530

    Hospital Management - Oauth2 - Login request and response dto updated.

--- src/main/java/com/hp/springsecurity/dto/LoginResponseDto.java
+++ src/main/java/com/hp/springsecurity/dto/LoginResponseDto.java
@@ -1,8 +1,15 @@
 package com.hp.springsecurity.dto;
 
+import com.hp.springsecurity.type.AuthProviderType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -10,6 +17,5 @@ import lombok.NoArgsConstructor;
 public class LoginResponseDto {
     private Long userId;
     private String accessToken;
-    private String refreshToken;
-
+    //private String refreshToken;
 }

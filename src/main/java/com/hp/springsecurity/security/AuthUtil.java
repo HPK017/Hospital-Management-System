b5994ee8f5f821b741d34728c094072079278090
HPK017 <parelharsh0250@gmail.com>
2026-02-26 19:50:32 +0530

    Hospital Management - Oauth2 - determine oauth2 username, providerId and providerType

--- src/main/java/com/hp/springsecurity/security/AuthUtil.java
+++ src/main/java/com/hp/springsecurity/security/AuthUtil.java
@@ -1,10 +1,13 @@
 package com.hp.springsecurity.security;
 
 import com.hp.springsecurity.entity.User;
+import com.hp.springsecurity.type.AuthProviderType;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -13,6 +16,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Component
 public class AuthUtil {
 
@@ -74,4 +78,44 @@ public class AuthUtil {
 //                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 2))
 //                .signWith(getSecretKey(), Jwts.SIG.HS256).compact();
 //    }
+
+    public AuthProviderType getProviderTypeFromRegistrationId(String registrationId) {
+        return switch (registrationId.toLowerCase()) {
+            case "google" -> AuthProviderType.GOOGLE;
+            case "github" -> AuthProviderType.GITHUB;
+            case "facebook" -> AuthProviderType.FACEBOOK;
+            default -> throw new IllegalArgumentException("Unsupported OAuth2 provider: " + registrationId);
+        };
+    }
+
+    public String determineProviderIdFromOAuthwUser(OAuth2User oAuth2User, String registrationId) {
+        String providerId = switch (registrationId.toLowerCase()) {
+            case "google" -> oAuth2User.getAttribute("sub");
+            case "github" -> oAuth2User.getAttribute("id").toString();
+            default -> {
+                log.error("Unsupported OAuth2 provider: {}", registrationId);
+                throw new IllegalArgumentException("Unsupported OAuth2 provider: " + registrationId);
+            }
+        };
+
+        if(providerId == null || providerId.isBlank()) {
+            log.error("Unable to determine providerId for provider: {}", registrationId);
+            throw new IllegalArgumentException("Unable to determine providerId for Oauth2 login");
+        }
+        return providerId;
+    }
+
+    public String determineUsernameFromOAuth2User(OAuth2User oAuth2User, String registrationId, String providerId) {
+        String email = oAuth2User.getAttribute("email");
+        if(email != null && !email.isBlank()) {
+            return email;
+        }
+
+        return switch (registrationId.toLowerCase()) {
+            case "google" -> oAuth2User.getAttribute("sub");
+            case "github" -> oAuth2User.getAttribute("login");
+            default -> providerId;
+        };
+
+    }
 }

7ac24900f5f59c3f327506e6324bd45e855e2346
HPK017 <parelharsh0250@gmail.com>
2026-03-02 21:38:49 +0530

    Hospital Management - admin role implementation

--- src/main/java/com/hp/springsecurity/controller/AdminController.java
+++ src/main/java/com/hp/springsecurity/controller/AdminController.java
@@ -1,13 +1,14 @@
 package com.hp.springsecurity.controller;
 
+import com.hp.springsecurity.dto.DoctorResponseDto;
+import com.hp.springsecurity.dto.OnBoardDoctorRequestDto;
 import com.hp.springsecurity.dto.PatientResponseDto;
+import com.hp.springsecurity.service.DoctorService;
 import com.hp.springsecurity.service.PatientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ import java.util.List;
 public class AdminController {
 
     private final PatientService patientService;
+    private final DoctorService doctorService;
 
 
     @GetMapping("/patients")
@@ -25,6 +27,11 @@ public class AdminController {
             @RequestParam(value = "size", defaultValue = "10") Integer pageSize
     ) {
         return ResponseEntity.ok(patientService.getAllPatients(pageNumber, pageSize));
+    }
+
+    @PostMapping("/onboardnew")
+    public ResponseEntity<DoctorResponseDto> onBoardNewDoctor(@RequestBody OnBoardDoctorRequestDto onBoardRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(doctorService.onBoardNewDoctor(onBoardRequestDTO));
     }
 
 }
--- src/main/java/com/hp/springsecurity/service/DoctorService.java
+++ src/main/java/com/hp/springsecurity/service/DoctorService.java
@@ -1,9 +1,13 @@
 package com.hp.springsecurity.service;
 
 import com.hp.springsecurity.dto.DoctorResponseDto;
-import com.hp.springsecurity.entity.Appointment;
-import com.hp.springsecurity.repository.AppointmentRepository;
+import com.hp.springsecurity.dto.OnBoardDoctorRequestDto;
+import com.hp.springsecurity.entity.Doctor;
+import com.hp.springsecurity.entity.User;
 import com.hp.springsecurity.repository.DoctorRepository;
+import com.hp.springsecurity.repository.UserRepository;
+import com.hp.springsecurity.type.RoleType;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -17,12 +21,32 @@ public class DoctorService {
 
     private final DoctorRepository doctorRepository;
     private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
 
     public List<DoctorResponseDto> getAllDoctors() {
         return doctorRepository.findAll()
                 .stream()
                 .map(doctor -> modelMapper.map(doctor, DoctorResponseDto.class))
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public DoctorResponseDto onBoardNewDoctor(OnBoardDoctorRequestDto onBoardDoctorRequestDto) {
+        User user = userRepository.findById(onBoardDoctorRequestDto.getUserId()).orElseThrow();
+
+        if(doctorRepository.existsById(onBoardDoctorRequestDto.getUserId())) {
+            throw new IllegalArgumentException("Already a doctor");
+        }
+
+        Doctor doctor = Doctor.builder()
+                .name(onBoardDoctorRequestDto.getName())
+                .specialization(onBoardDoctorRequestDto.getName())
+                .user(user)
+                .build();
+
+        user.getRoles().add(RoleType.DOCTOR);
+
+        return modelMapper.map(doctorRepository.save(doctor), DoctorResponseDto.class);
     }
 
 

1870a42c7122edf825825d9044b750de61eb92ed
HPK017 <parelharsh0250@gmail.com>
2026-03-07 19:26:47 +0530

    Hospital Management - role given to particular service implementation.

--- src/main/java/com/hp/springsecurity/service/AppointmentService.java
+++ src/main/java/com/hp/springsecurity/service/AppointmentService.java
@@ -12,6 +12,8 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,6 +29,7 @@ public class AppointmentService {
     private final ModelMapper modelMapper;
 
     @Transactional
+    @Secured("ROLE_PATIENT")
     public AppointmentResponseDto createNewAppointment(CreateAppointmentRequestDto createAppointmentRequestDto){
         Long doctorId = createAppointmentRequestDto.getDoctorId();
         Long patientId = createAppointmentRequestDto.getPatientId();
@@ -52,6 +55,7 @@ public class AppointmentService {
     }
 
     @Transactional
+    @PreAuthorize("hasAuthority('appointment:write') or #doctorId == authentication.principal.id)")
     public Appointment reassignAppointmentToAnotherDoctor(Long appointmentId, Long doctorId) {
         Appointment appointment = appointmentRepository.findById(appointmentId).orElseThrow();
         Doctor doctor = doctorRepository.findById(doctorId).orElseThrow();
@@ -65,6 +69,7 @@ public class AppointmentService {
 
 
     @Transactional
+    @PreAuthorize("hasRole('ADMIN') OR (hasRole('DOCTOR') AND #doctorId == authentication.principal.id )")
     public List<AppointmentResponseDto> getAllAppointmentsOfDoctor(Long doctorId) {
         Doctor doctor = doctorRepository.findById(doctorId).
                 orElseThrow(() -> new EntityNotFoundException("Doctor not found with Id" + doctorId));
